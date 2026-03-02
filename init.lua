7d9a5311e9aa018076b93666c7d45a454e45755a
Jade Iden <jiden3@gmail.com>
2026-03-02 13:08:02 -0500

    Added support for computer-local config

--- init.lua
+++ init.lua
@@ -2,5 +2,8 @@ require("config.lazy")
 require("config.settings")
 require("keybinds")
 
+-- If computer-local config exists, import it
+pcall(function () require("config.local") end)
+
 vim.cmd.colorscheme "tokyonight"
 
