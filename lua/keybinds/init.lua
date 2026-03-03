839648a57f71ae859d8108a5799feafaa9261a06
Jade Iden <jiden3@gmail.com>
2026-03-03 12:54:47 -0500

    Removed duplicate Noegit def

--- lua/keybinds/init.lua
+++ lua/keybinds/init.lua
@@ -1,7 +1,5 @@
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-vim.keymap.set('n', '<leader>gg', ':Neogit<CR>', { desc = 'Open Neogit' })
-vim.keymap.set('n', '<leader>bb', ':Neotree buffers<CR>', { desc = 'Open neotree [b]uffer menu' })
 vim.keymap.set('n', '<leader>bd', ':bd<CR>', { desc = '[D]elete [buffer]' })
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
